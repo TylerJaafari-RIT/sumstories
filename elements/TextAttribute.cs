46c6f20cb6c2015d20ec11ea95bdbe8c974cc5f2
Tyler <txj3401@rit.edu>
2026-03-03 13:08:39 -0500

    Console app in progress.

--- elements/TextAttribute.cs
+++ elements/TextAttribute.cs
@@ -7,12 +7,13 @@ namespace sumstories.elements;
 internal class TextAttribute : Attribute {
 	Accuracy Accuracy { get; set; }
 
-	public TextAttribute(string Name, AttributeValue value) {
-		base(Name, value);
+	public TextAttribute(string Name, AttributeValue Value): base(Name, Value) {
+		
 	}
 
-	public TextAttribute(string Name, AttributeValue value, Accuracy accuracy) {
-		object value1 = base(Name, value);
+	public TextAttribute(string Name, AttributeValue Value, Accuracy accuracy): base(Name, Value) {
+		this.Name = Name;
+		this.Value = Value;
 		this.Accuracy = accuracy;
 	}
 }
